32d281c97df14152388b202bd93b93b80dba4a53
Robert Brewer <65512319+robertbrewer84@users.noreply.github.com>
2023-11-08 15:09:03 +0000

    Deleted directory

---
+++ front-end/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { OutputService } from './output.service';
+import { Output } from './output'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit {
+  
+  title = 'visual-bioinformatics';
+
+  text = '...';
+
+  constructor(private outputService: OutputService) { }
+
+  ngOnInit() {
+    this.getOutputs()
+  }
+
+  getOutputs() {
+    this.outputService.getOutputs().subscribe((data: Output) => this.text = data.title);
+  }
+
+}
---
+++ front-end/src/app/output.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { Output } from './output'
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class OutputService {
+
+  constructor(private http: HttpClient) { }
+
+  getOutputs(): Observable<Output> {
+    return this.http.get<Output>('http://127.0.0.1:5000/');
+  }
+  
+}

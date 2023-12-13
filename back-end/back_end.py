8b052ff7fb274bb75c05c348124ffdf5e9a84d65
Robert Brewer <65512319+robertbrewer84@users.noreply.github.com>
2023-12-13 17:04:59 +0000

    Added basic tests for back-end

---
+++ back-end/back_end.py
@@ -0,0 +1,73 @@
+import json
+from flask import Flask, jsonify
+from flask_cors import CORS
+import werkzeug.exceptions
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import copy
+sc.settings.verbosity = 3
+
+##app = Flask(__name__)
+##CORS(app)
+
+def create_app():
+    
+    app = Flask(__name__)
+    CORS(app)
+  
+    data = {
+        'pbmc3k': None,
+        'filtered' : None
+    }
+
+    @app.route('/loaddata/')
+    def loaddata():
+        if data['pbmc3k'] is None:
+            data['pbmc3k'] = sc.read_10x_mtx('data/filtered_gene_bc_matrices/hg19/', var_names='gene_symbols', cache=True) 
+            data['pbmc3k'].var_names_make_unique()
+        message = {
+            'text': str(data['pbmc3k']),
+            'other': ''
+        }
+        return jsonify(message)
+
+    @app.route('/basicfiltering/<min_genes>/<min_cells>/')
+    def basicfiltering(min_genes, min_cells):
+        if data['pbmc3k'] is None:
+            raise IncorrectOrderException()
+        else:
+            data['filtered'] = copy.copy(data['pbmc3k'])
+            sc.pp.filter_cells(data['filtered'], min_genes = int(min_genes))
+            sc.pp.filter_genes(data['filtered'], min_cells = int(min_cells))
+            message = {
+                'text': str(data['filtered']),
+                'other': ''
+            }
+            return jsonify(message)
+    
+    class IncorrectOrderException(werkzeug.exceptions.HTTPException):
+        code = 406
+        description = 'The blocks you have executed are not a valid order. Please check the order and try again.'
+
+    def handle_exception(e):
+        """Return JSON instead of HTML for HTTP errors."""
+        response = e.get_response()
+        response.data = json.dumps({
+            "code": e.code,
+            "name": e.name,
+            "description": e.description,
+        })
+        response.content_type = "application/json"
+        return response
+
+    app.register_error_handler(IncorrectOrderException, handle_exception)
+
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+
+    app.run()
+
---
+++ back-end/test.py
@@ -0,0 +1,50 @@
+import pytest
+from back_end import create_app
+from unittest.mock import patch
+import anndata
+import json
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+@patch('scanpy.read_10x_mtx')
+def test_loaddata(mock, client):
+    mock.return_value = anndata.AnnData()
+    response = client.get('/loaddata/')
+    assert response.status_code == 200
+    message = {
+            'text': "AnnData object with n_obs × n_vars = 0 × 0",
+            'other': ''
+    }
+    assert json.loads(response.data) == message
+
+def test_basicfiltering(client):
+
+    response = client.get('/basicfiltering/1/1/')
+    assert response.status_code == 406
+    message = {
+        "code": 406,
+        "name": 'Not Acceptable',
+        "description": 'The blocks you have executed are not a valid order. Please check the order and try again.',
+    }
+    assert json.loads(response.data) == message

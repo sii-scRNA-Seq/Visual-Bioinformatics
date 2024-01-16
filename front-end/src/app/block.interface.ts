071c77002470e2f47224fc1a71ae5ba0ee964393
Robert Brewer <65512319+robertbrewer84@users.noreply.github.com>
2024-01-16 11:07:15 +0000

    Linted front end

--- front-end/src/app/block.interface.ts
+++ front-end/src/app/block.interface.ts
@@ -1,18 +1,18 @@
-import { Observable } from "rxjs"
+import { Observable } from 'rxjs';
 
 export interface Block {
-    blockId: BlockId
-    title: string
-    possibleChildBlocks: BlockId[]
-    parameters: Parameter[]
-    onRun: (block: Block) => Observable<unknown>
+  blockId: BlockId
+  title: string
+  possibleChildBlocks: BlockId[]
+  parameters: Parameter[]
+  onRun: (block: Block) => Observable<unknown>
 }
 
 export type BlockId = 'loaddata' | 'basicfiltering';
 
 export interface Parameter {
-    key: string
-    text: string
-    value: number
+  key: string
+  text: string
+  value: number
 }
   

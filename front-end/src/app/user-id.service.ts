08c331712d4e96bbb01d518dbe7ade0cc7d034c0
Robert Brewer <65512319+robertbrewer84@users.noreply.github.com>
2024-02-06 16:45:13 +0000

    Update user-id.service.ts

--- front-end/src/app/user-id.service.ts
+++ front-end/src/app/user-id.service.ts
@@ -2,7 +2,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 import { ClientService } from './client.service';
-import { UserIdServiceInterface } from './user-id-service-interface';
+import { UserIdServiceInterface } from './user-id.service.interface';
 
 @Injectable({
   providedIn: 'root'

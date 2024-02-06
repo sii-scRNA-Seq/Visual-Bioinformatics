cd4881eccacd150cf59d67ff2963ee1dcd9074c0
Robert Brewer <65512319+robertbrewer84@users.noreply.github.com>
2024-02-06 16:12:42 +0000

    Removed constructor from MockUserIdService

--- front-end/src/app/mock-user-id.service.ts
+++ front-end/src/app/mock-user-id.service.ts
@@ -10,10 +10,6 @@ export class MockUserIdService implements UserIdServiceInterface {
 
   private readonly userId$: BehaviorSubject<string | null> = new BehaviorSubject<string | null> (null);
   readonly userId: Observable<string | null> = this.userId$.asObservable();
-  
-  constructor() {
-    // Do nothing
-  }
 
   async setUserId(): Promise<void> {
     this.userId$.next('mock_user_id');

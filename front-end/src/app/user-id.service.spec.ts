f18cd749af80fe9992b717359c9eb881bb465b4d
Robert Brewer <65512319+robertbrewer84@users.noreply.github.com>
2024-02-07 15:42:07 +0000

    Kept linter happy

--- front-end/src/app/user-id.service.spec.ts
+++ front-end/src/app/user-id.service.spec.ts
@@ -14,7 +14,7 @@ describe('UserIdService', () => {
       imports: [ 
         HttpClientTestingModule,
         MatSnackBarModule,
-       ]
+      ]
     });
     service = TestBed.inject(UserIdService);
   });

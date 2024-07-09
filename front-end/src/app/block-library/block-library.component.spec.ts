8121b5cd7cd5612a9979ff9b002a76657f1978d4
Robert Brewer <robertbrewer001@gmail.com>
2024-07-09 13:42:38 +0100

    Update and add tests for block library buttons

--- front-end/src/app/block-library/block-library.component.spec.ts
+++ front-end/src/app/block-library/block-library.component.spec.ts
@@ -54,21 +54,13 @@ describe('BlockLibraryComponent', () => {
     it ('should be available when blocks are not being executed', () => {
       component.executingBlocks = false;
       fixture.detectChanges();
-      const blockIDs: string[] = ['loaddata', 'basicfiltering', 'qcplots', 'qcfiltering', 'variablegenes', 'pca', 'integration', 'runumap'];
-      blockIDs.forEach(blockID => {
-        expect(fixture.debugElement.query(By.css('#'.concat(blockID))).nativeElement.disabled).toEqual(false);
-      });
+      expect(fixture.debugElement.query(By.css('#loaddata')).nativeElement.disabled).toEqual(false);
     });
 
     it ('should become disabled while blocks are being executed', () => {
-      component.executingBlocks = false;
+      component.executingBlocks = true;
       fixture.detectChanges();
       const blockIDs: string[] = ['loaddata', 'basicfiltering', 'qcplots', 'qcfiltering', 'variablegenes', 'pca', 'integration', 'runumap'];
-      blockIDs.forEach(blockID => {
-        expect(fixture.debugElement.query(By.css('#'.concat(blockID))).nativeElement.disabled).toEqual(false);
-      });
-      component.executingBlocks = true;
-      fixture.detectChanges(); 
       blockIDs.forEach(blockID => {
         expect(fixture.debugElement.query(By.css('#'.concat(blockID))).nativeElement.disabled).toEqual(true);
       });
@@ -77,14 +69,42 @@ describe('BlockLibraryComponent', () => {
     it ('should become available once blocks have stopped being executed', () => {
       component.executingBlocks = true;
       fixture.detectChanges();
-      const blockIDs: string[] = ['loaddata', 'basicfiltering', 'qcplots', 'qcfiltering', 'variablegenes', 'pca', 'integration', 'runumap'];
-      blockIDs.forEach(blockID => {
-        expect(fixture.debugElement.query(By.css('#'.concat(blockID))).nativeElement.disabled).toEqual(true);
-      });
       component.executingBlocks = false;
       fixture.detectChanges();
-      blockIDs.forEach(blockID => {
+      expect(fixture.debugElement.query(By.css('#loaddata')).nativeElement.disabled).toEqual(false);
+    });
+
+    it ('should only allow Load Data block to be added when canvas is empty', () => {
+      component.executingBlocks = false;
+      component.blockList = [];
+      fixture.detectChanges();
+      const enabledBlocks: string[] = ['loaddata'];
+      const disabledBlocks: string[] = ['basicfiltering', 'qcplots', 'qcfiltering', 'variablegenes', 'pca', 'integration', 'runumap'];
+      enabledBlocks.forEach(blockID => {
         expect(fixture.debugElement.query(By.css('#'.concat(blockID))).nativeElement.disabled).toEqual(false);
+      });
+      disabledBlocks.forEach(blockID => {
+        expect(fixture.debugElement.query(By.css('#'.concat(blockID))).nativeElement.disabled).toEqual(true);
+      });
+    });
+
+    it ('should only allow possible child blocks of the last block on the canvas to be added when canvas is not empty', () => {
+      component.executingBlocks = false;
+      component.blockList = [{
+        blockId: 'loaddata',
+        blockUUID: '',
+        title: 'Load Data',
+        possibleChildBlocks: ['basicfiltering', 'qcplots', 'qcfiltering', 'variablegenes'],
+        parameters: [],
+      }];
+      fixture.detectChanges();
+      const enabledBlocks: string[] = ['basicfiltering', 'qcplots', 'qcfiltering', 'variablegenes'];
+      const disabledBlocks: string[] = ['loaddata', 'pca', 'integration', 'runumap'];
+      enabledBlocks.forEach(blockID => {
+        expect(fixture.debugElement.query(By.css('#'.concat(blockID))).nativeElement.disabled).toEqual(false);
+      });
+      disabledBlocks.forEach(blockID => {
+        expect(fixture.debugElement.query(By.css('#'.concat(blockID))).nativeElement.disabled).toEqual(true);
       });
     });
   });

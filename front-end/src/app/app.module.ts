2cbfdd8981d11b2ffc4cd94a42773bc6cf84183b
Robert Brewer <robertbrewer001@gmail.com>
2024-07-29 15:24:01 +0100

    Replace other buttons with Material buttons

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -31,6 +32,7 @@ bootstrap: [AppComponent], imports: [BrowserModule,
   MatButtonModule,
   MatCardModule,
   MatFormFieldModule,
+  MatIconModule,
   MatProgressSpinnerModule,
   MatSelectModule,
   MatSnackBarModule,

4a41a5c88bf4263be2ad00a78545e85697e693fb
Robert Brewer <robertbrewer001@gmail.com>
2024-08-07 13:46:03 +0100

    Remove indents

--- make_release.sh
+++ make_release.sh
@@ -10,35 +10,35 @@ git fetch
 
 if [ $(git tag -l "$version") ]; then
 
-    git checkout tags/$version
-    git pull
+git checkout tags/$version
+git pull
 
-    cd front-end/
-    npm install
-    npm run build
-    cd ..
+cd front-end/
+npm install
+npm run build
+cd ..
 
-    pgrep -f back_end.py | awk '{print "kill -9 " $1}' | sh
+pgrep -f back_end.py | awk '{print "kill -9 " $1}' | sh
 
-    rm -r back-end/dist
-    cp -r front-end/dist back-end/
+rm -r back-end/dist
+cp -r front-end/dist back-end/
 
-    tempfile2=$(mktemp)
-    cat > $tempfile2 <<EOF
-    conda deactivate
-    conda env remove -n scampi
-    conda env create -f ../environment.yml
-    conda activate scampi
-    python back_end.py --production-mode
-    EOF
-    screen -S scampi -X readbuf $tempfile2
-    screen -S scampi -X paste .
-    rm -f $tempfile2
+tempfile2=$(mktemp)
+cat > $tempfile2 <<EOF
+conda deactivate
+conda env remove -n scampi
+conda env create -f ../environment.yml
+conda activate scampi
+python back_end.py --production-mode
+EOF
+screen -S scampi -X readbuf $tempfile2
+screen -S scampi -X paste .
+rm -f $tempfile2
 
-    echo "The app is being started in screen, you can monitor this via htop"
+echo "The app is being started in screen, you can monitor this via htop"
 
 else
 
-    echo "The given version does not exist"
+echo "The given version does not exist"
 
 fi

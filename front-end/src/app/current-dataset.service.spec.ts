e600a5354c7e187c8e178119911108eecd01a4be
Robert Brewer <robertbrewer001@gmail.com>
2024-08-14 17:54:36 +0100

    Update current-dataset.service.spec.ts

--- front-end/src/app/current-dataset.service.spec.ts
+++ front-end/src/app/current-dataset.service.spec.ts
@@ -1,13 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CurrentDatasetService } from './current-dataset.service';
+import { DatasetInfoService } from './dataset-info.service';
+import { MockDatasetInfoService } from './mock-dataset-info.service';
 
 describe('CurrentDatasetService', () => {
   let service: CurrentDatasetService;
+  // let datasetInfoService: DatasetInfoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: DatasetInfoService, useClass: MockDatasetInfoService },
+      ]
+    });
     service = TestBed.inject(CurrentDatasetService);
+    // datasetInfoService = TestBed.inject(DatasetInfoService);
   });
 
   it('should be created', () => {

dacfbe0ad4f0f3260fcb61c9687c3ee0937b7295
Robert Brewer <robertbrewer001@gmail.com>
2024-08-19 17:04:07 +0100

    reStructuredText for the Block class

--- back-end/app/block/block_interface.py
+++ back-end/app/block/block_interface.py
@@ -8,12 +8,34 @@ def adata_text(adata: AnnData) -> str:
 
 
 class Block:
+
+    """
+    Block superclass. Should only be instantiated through subclasses and never directly.
+
+    Contains two methods:
+
+        - validate_parameters() validates that all of the required parameters are present in the given dictionary and is provided concretely.
+        - run() executes the code expected for a particular block and is provided abstractly.
+    """
+
     required_parameters = []
+    """The parameters required by a particular block."""
+
 
     def __init__(self):
+        """Initialise a `Block` object."""
         pass
 
     def validate_parameters(self, parameters: dict) -> None:
+        """
+        Validate that all of the parameters in the required_parameters attribute are also present in the parameters dictionary. Raise an exception if this is not the case.
+
+        Parameters:
+
+            - `parameters`: a dictionary mapping block parameters to their values.
+
+        Exceptions: `MissingParametersException`.
+        """
         missing_parameters = []
         for param in self.required_parameters:
             if param not in parameters:
@@ -22,4 +44,17 @@ class Block:
             raise MissingParametersException("Missing parameters: " + json.dumps(missing_parameters))
 
     def run(self, adata: AnnData, parameters: dict) -> (AnnData, dict):
+        """
+        Execute the code for a particular block.
+
+        Parameters:
+
+            - `adata`: the AnnData for which the code should be executed.
+            - `parameters`: a dictionary mapping parameter names to their values, which should be used while executing the code.
+
+        Return:
+
+            - The resulting AnnData after performing the block's behaviour.
+            - A dictionary containing the results that will be seen by the user.
+        """
         raise NotImplementedError()

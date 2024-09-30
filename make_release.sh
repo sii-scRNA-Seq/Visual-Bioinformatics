b15b261da15de6e9a23a2f8b988ac96389b1dd9f
Andrew McCluskey <andrew@cluskii.com>
2024-09-30 11:42:39 +0100

    Add celery and redis setup to release script

--- make_release.sh
+++ make_release.sh
@@ -18,16 +18,59 @@ npm install
 npm run build
 cd ..
 
+# Stop app
 pgrep -f back_end.py | awk '{print "kill -9 " $1}' | sh
+pgrep -f redis | awk '{print "kill -9 " $1}' | sh
+pgrep -f celery | awk '{print "kill -9 " $1}' | sh
+
+# Stop conda env
+tempfile2=$(mktemp)
+cat > $tempfile2 <<EOF
+conda deactivate
+EOF
+screen -S scampi -X readbuf $tempfile2
+screen -S scampi -X paste .
+screen -S redis -X readbuf $tempfile2
+screen -S redis -X paste .
+screen -S celery -X readbuf $tempfile2
+screen -S celery -X paste .
+rm -f $tempfile2
 
 rm -r back-end/dist
 cp -r front-end/dist back-end/
 
-tempfile2=$(mktemp)
-cat > $tempfile2 <<EOF
-conda deactivate
+# Rebuild conda environment
 conda env remove -n scampi -y
 conda env create -f ../environment.yml
+
+# Restart Redis
+# WD: back-end
+tempfile2=$(mktemp)
+cat > $tempfile2 <<EOF
+conda activate scampi
+chmod +x start_redis.sh
+./start_redis.sh
+EOF
+screen -S redis -X readbuf $tempfile2
+screen -S redis -X paste .
+rm -f $tempfile2
+
+# Restart Celery Worker
+# WD: back-end/app
+tempfile2=$(mktemp)
+cat > $tempfile2 <<EOF
+conda activate scampi
+chmod +x ../start_celery_worker.sh
+../start_celery_worker.sh
+EOF
+screen -S celery -X readbuf $tempfile2
+screen -S celery -X paste .
+rm -f $tempfile2
+
+# Restart Server
+# WD: back-end/app
+tempfile2=$(mktemp)
+cat > $tempfile2 <<EOF
 conda activate scampi
 python back_end.py --production-mode
 EOF
